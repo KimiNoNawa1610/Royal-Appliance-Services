1a8c97e412cfd4967e8d3edb153feb3981d6ef16
brianhuyphung <brianhuyphung@gmail.com>
2022-10-17 20:06:17 -0700

    create job gui
    added code for create job page

--- front-end/App.js
+++ front-end/App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CreateJob from './pages/CreateJob.js';
 import Login from "./pages/Login.js"
 
 
 export default function App() {
   //Navigation is not in place, using this to test each different page manually
   return (
+    // <View style={styles.container}>
+    //   <Login/>
+    //   <StatusBar style="auto" />
+
+    // </View>
+
     <View style={styles.container}>
-      <Login/>
+      <CreateJob/>
       <StatusBar style="auto" />
 
     </View>
