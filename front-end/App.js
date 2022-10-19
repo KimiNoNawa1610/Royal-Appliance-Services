178e6ee87fb54f3e95aaeb0596a955d271674cad
Geovanny Padilla <geovanny.padilla@student.csulb.edu>
2022-10-18 20:46:26 -0700

    Edit Job progress

--- front-end/App.js
+++ front-end/App.js
@@ -2,15 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from "./pages/Login.js"
+import EditJob from "./pages/EditJob";
 
 
 export default function App() {
   //Navigation is not in place, using this to test each different page manually
   return (
     <View style={styles.container}>
-      <Login/>
+      <EditJob/>
       <StatusBar style="auto" />
-
     </View>
   );
 }
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: '#27445C',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- front-end/pages/EditJob.js
+++ front-end/pages/EditJob.js
@@ -1,16 +1,137 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Button,
     StyleSheet,
     TextInput,
+    Text,
     View,
-    Alert}
+    VirtualizedList,
+    Alert, StatusBar
+}
     from 'react-native';
 
-const EditJob = () => {
+const DATA =
+    [
+        {detailTitle : "PH: PreExsiting_Detail_0"},
+        {detailTitle : "PH: PreExsiting_Detail_1"},
+        {detailTitle : "PH: PreExsiting_Detail_2"},
+        {detailTitle : "PH: PreExsiting_Detail_0"},
+        {detailTitle : "PH: PreExsiting_Detail_1"},
+        {detailTitle : "PH: PreExsiting_Detail_2"},
+        {detailTitle : "PH: PreExsiting_Detail_0"},
+        {detailTitle : "PH: PreExsiting_Detail_1"},
+        {detailTitle : "PH: PreExsiting_Detail_2"},
+        {detailTitle : "PH: PreExsiting_Detail_0"},
+        {detailTitle : "PH: PreExsiting_Detail_1"},
+        {detailTitle : "PH: PreExsiting_Detail_2"},
+        {detailTitle : "PH: PreExsiting_Detail_0"},
+        {detailTitle : "PH: PreExsiting_Detail_3"}
+    ];
 
-
-
+const getPreData = () => {
+    /* Reference
+    Invoice =
+    {
+        id
+        total
+        my_part
+        labor
+        tax
+        shipping
+        net
+        part_installed
+        client_sell
+        paid_by
+    }
+    */
+    //TODO: Call to the backend to get pre-existing data on a {Job}
 }
+
+const getJobDetail = (data, index) => {
+    return data[index];
+};
+
+const getJobDetailCount = (data) =>{
+    return data.length;
+}
+
+
+
+const JobDetail = ({jobDetail}) => {
+    const [detail, onChangeDetail] = React.useState(jobDetail["detailTitle"]);
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Job Detail</Text>
+            <TextInput
+                style={styles.input}
+                value={detail}
+                onChangeText={onChangeDetail}
+                placeholderTextColor={'#fff'}
+            />
+        </View>
+    )};
+
+
+
+const EditJob = () => {
+    return (
+        <View>
+            <View name = "Text" style={styles.title}>
+                <Text style={styles.bigText}>Edit Job</Text>
+            </View>
+
+
+            <View name="Pre-Existing Data" style={styles.existing}>
+                <VirtualizedList
+                    data={DATA}
+                    initialNumToRender={4}
+                    renderItem={   ({ item }) => <JobDetail jobDetail={item} />   }
+                    keyExtractor={item => item.key}
+                    getItem={getJobDetail}
+                    getItemCount={getJobDetailCount}
+                />
+            </View>
+
+
+            <View name = "Data to Add">
+
+            </View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight,
+    },
+    existing:{
+        borderRadius: 5,
+        backgroundColor: '#163352',
+        padding: 15,
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10,
+        borderColor: '#fff',
+        color: '#fff',
+    },
+    title: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 15,
+        color: '#fff'
+    },
+    bigText: {
+        fontSize: 32,
+        color: '#fff'
+    }
+});
 
 export default EditJob;
