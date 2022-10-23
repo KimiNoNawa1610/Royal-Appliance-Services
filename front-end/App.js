42085d457db635e23ac29a901807c2ab6eb2e0ab
Geovanny Padilla <geovanny.padilla@student.csulb.edu>
2022-10-23 15:37:24 -0700

    Edit Job inits
    Started Edit job, now need to change it to reflect the JSON string it will receive

--- front-end/App.js
+++ front-end/App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CreateJob from './pages/CreateJob.js';
-import Login from "./pages/Login.js"
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CreateJob from "./pages/CreateJob.js";
+import Login from "./pages/Login.js";
 import EditJob from "./pages/EditJob";
 
+/*
+{
+    "client": "NVIDIA",
+    "rows":[
+    {"item":"RTX 3080","total":320,"my_part":30,"labor":150,"tax":30.5,"sell":699,"shipping":11,"paid_by":"cash"},
+    {"item":"RTX 3070","total":2368.25,"my_part":45,"labor":150,"tax":25.45,"sell":499,"shipping":11,"paid_by":"credit"}
+    ],
+    "paid":670,
+    "note":""
+}
+ */
 
 export default function App() {
   //Navigation is not in place, using this to test each different page manually
@@ -16,7 +27,7 @@ export default function App() {
     // </View>
 
     <View style={styles.container}>
-      <CreateJob/>
+      <EditJob />
       <StatusBar style="auto" />
     </View>
   );
@@ -25,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#27445C',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#27445C",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- front-end/pages/EditJob.js
+++ front-end/pages/EditJob.js
@@ -1,38 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState, Componenet } from "react";
 import {
-    Button,
-    StyleSheet,
-    TextInput,
-    Text,
-    View,
-    VirtualizedList,
-    Alert, StatusBar
-}
-    from 'react-native';
+  Button,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  VirtualizedList,
+  Alert,
+  StatusBar,
+  Modal,
+  Pressable,
+} from "react-native";
 
-const DATA =
-    [
-        {detailTitle : "PH: PreExsiting_Detail_0"},
-        {detailTitle : "PH: PreExsiting_Detail_1"},
-        {detailTitle : "PH: PreExsiting_Detail_2"},
-        {detailTitle : "PH: PreExsiting_Detail_0"},
-        {detailTitle : "PH: PreExsiting_Detail_1"},
-        {detailTitle : "PH: PreExsiting_Detail_2"},
-        {detailTitle : "PH: PreExsiting_Detail_0"},
-        {detailTitle : "PH: PreExsiting_Detail_1"},
-        {detailTitle : "PH: PreExsiting_Detail_2"},
-        {detailTitle : "PH: PreExsiting_Detail_0"},
-        {detailTitle : "PH: PreExsiting_Detail_1"},
-        {detailTitle : "PH: PreExsiting_Detail_2"},
-        {detailTitle : "PH: PreExsiting_Detail_0"},
-        {detailTitle : "PH: PreExsiting_Detail_3"}
-    ];
+import SelectList from "react-native-dropdown-select-list/index";
+
+
+const DATA = [
+  { detailTitle: "PH: PreExsiting_Detail_0" },
+  { detailTitle: "PH: PreExsiting_Detail_1" },
+  { detailTitle: "PH: PreExsiting_Detail_2" },
+  { detailTitle: "PH: PreExsiting_Detail_0" },
+  { detailTitle: "PH: PreExsiting_Detail_1" },
+  { detailTitle: "PH: PreExsiting_Detail_2" },
+  { detailTitle: "PH: PreExsiting_Detail_0" },
+
+  { detailTitle: "PH: PreExsiting_Detail_3" },
+];
 
 const getPreData = () => {
-    /* Reference
+  /* Reference
     Invoice =
     {
-        id
+        id : {name : "value",
+              value : "value"
+        }
+    }
         total
         my_part
         labor
@@ -44,94 +46,196 @@ const getPreData = () => {
         paid_by
     }
     */
-    //TODO: Call to the backend to get pre-existing data on a {Job}
-}
-
-const getJobDetail = (data, index) => {
-    return data[index];
+  //TODO: Call to the backend to get pre-existing data on a {Job}
 };
 
-const getJobDetailCount = (data) =>{
-    return data.length;
-}
+const getJobDetail = (data, index) => {
+  return data[index];
+};
+
+const getJobDetailCount = (data) => {
+  return data.length;
+};
+
+const JobDetail = ({ jobDetail }) => {
+  const [detail, onChangeDetail] = React.useState(jobDetail["detailTitle"]);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Job Detail</Text>
+      <TextInput
+        style={styles.input}
+        value={detail}
+        onChangeText={onChangeDetail}
+        placeholderTextColor={"#fff"}
+      />
+    </View>
+  );
+};
+
+const AddJobDetailModal = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selected, setSelected] = React.useState("");
+  let detailType = [
+    {key: '1', value: 'Part'},
+    {key: '2', value: 'Other Detail'}
+  ];
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
 
 
-
-const JobDetail = ({jobDetail}) => {
-    const [detail, onChangeDetail] = React.useState(jobDetail["detailTitle"]);
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Job Detail</Text>
-            <TextInput
-                style={styles.input}
-                value={detail}
-                onChangeText={onChangeDetail}
-                placeholderTextColor={'#fff'}
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>New Detail</Text>
+            <SelectList
+                search = {false}
+                setSelected={setSelected}
+                data={detailType}
+                onsSelect={()=>alert(selected)}
             />
+            <Text style={styles.addJobDetailText}>Job Detail</Text>
+            <TextInput
+                style={styles.addJobDetailInput}
+            />
+
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Finish</Text>
+            </Pressable>
+          </View>
+
+
+
         </View>
-    )};
+      </Modal>
 
 
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Add New Detail</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const EditJob = () => {
-    return (
-        <View>
-            <View name = "Text" style={styles.title}>
-                <Text style={styles.bigText}>Edit Job</Text>
-            </View>
+  return (
+    <View>
+      <View name="Text" style={styles.title}>
+        <Text style={styles.bigText}>Edit Job</Text>
+      </View>
 
+      <View name="Pre-Existing Data" style={styles.existing}>
+        <VirtualizedList
+          data={DATA}
+          initialNumToRender={4}
+          renderItem={({ item }) => <JobDetail jobDetail={item} />}
+          keyExtractor={(item) => item.key}
+          getItem={getJobDetail}
+          getItemCount={getJobDetailCount}
+        />
+      </View>
 
-            <View name="Pre-Existing Data" style={styles.existing}>
-                <VirtualizedList
-                    data={DATA}
-                    initialNumToRender={4}
-                    renderItem={   ({ item }) => <JobDetail jobDetail={item} />   }
-                    keyExtractor={item => item.key}
-                    getItem={getJobDetail}
-                    getItemCount={getJobDetailCount}
-                />
-            </View>
-
-
-            <View name = "Data to Add">
-
-            </View>
-
-        </View>
-    );
-}
+      <View name="Data to Add">
+        <AddJobDetailModal />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight,
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  existing: {
+    borderRadius: 5,
+    backgroundColor: "#163352",
+    padding: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    borderColor: "#fff",
+    color: "#fff",
+  },
+  title: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 15,
+    color: "#fff",
+  },
+  bigText: {
+    fontSize: 32,
+    color: "#fff",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    width: 300,
+    height: 500,
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    existing:{
-        borderRadius: 5,
-        backgroundColor: '#163352',
-        padding: 15,
-    },
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        borderRadius: 5,
-        padding: 10,
-        borderColor: '#fff',
-        color: '#fff',
-    },
-    title: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        fontSize: 15,
-        color: '#fff'
-    },
-    bigText: {
-        fontSize: 32,
-        color: '#fff'
-    }
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#2196F3",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  addJobDetailText:{
+    paddingTop: 20,
+  },
+  addJobDetailInput:{
+    paddingBottom: 20
+  }
+
 });
 
 export default EditJob;
