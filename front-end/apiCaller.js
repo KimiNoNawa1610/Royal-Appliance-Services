28bf97aed2c15f075b5d3e2b13aaf513e2c9589d
brianhuyphung <brianhuyphung@gmail.com>
2022-11-15 15:49:28 -0800

    Update apiCaller.js

--- front-end/apiCaller.js
+++ front-end/apiCaller.js
@@ -1,24 +1,18 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { BASE_URL } from "./config";
 
-export const getJobs = async(supportCredentials) => {
+
+export const getJobs = async() => {
+    const token1 = await AsyncStorage.getItem("AccessToken");
     try {
       //query a join table at endpoint and then retrieve that data
-      const res = await axios.get("/get_jobs/1/2022-11-04/2022-11-06", supportCredentials, {timeout: 10000});
+      const res = await axios.get(BASE_URL + "/get_jobs/1/2022-11-04/2022-11-06/", {
+        headers: { token: token1 },
+      });
       console.log(res)
       return res.data;
     } catch (err) {
       return err;
     }
   };
-
-  
-// export const loginCall = async (userCredential, dispatch) => {
-//     dispatch({ type: "LOGIN_START" });
-//     try {
-//       const res = await axios.post("/auth/login", userCredential);
-//       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
-//     } catch (err) {
-//       dispatch({ type: "LOGIN_FAILURE", payload: err });
-//       alert("Invalid email and/or password. Try again.");
-//     }
-//   };
