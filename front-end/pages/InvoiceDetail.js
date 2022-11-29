b33d459e93a04ccb288e5f09aa4a235bd124d6a5
KimiNoNawa1610 <vothanhnhan108@gmail.com>
2022-11-28 22:54:33 -0800

    update api

--- front-end/pages/InvoiceDetail.js
+++ front-end/pages/InvoiceDetail.js
@@ -1,45 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, Image, ScrollView, WebView } from "react-native";
 import axios from "axios";
 import { BASE_URL } from "../config";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { StorageAccessFramework } from 'expo-file-system';
 import {
   Button,
+  Layout,
+  Text,
+  Divider,
 } from "@ui-kitten/components";
-import { showMessage } from "react-native-flash-message";
+import * as MediaLibrary from "expo-media-library";
+import * as FileSystem from 'expo-file-system';
+import { StorageAccessFramework } from 'expo-file-system';
+
 
 
 const InvoiceDetail = ({ item, setVisible }) => {
 
-  const [source, setSource] = useState("");
+  const [imageSource, Setsource] = useState("");
 
-  useEffect(() => { getInvoice(); }, []);
-
-  const getInvoice = async () => {
-    const token1 = await AsyncStorage.getItem("AccessToken");
-    //console.log(token1)
-    axios
-      .get(BASE_URL + "/get_invoice/" + item["invoiceID"].toString() + "/base64", {
-        headers: { token: token1 }
-      })
-      .then(async (res1) => {
-        //console.log(res1.data)
-        setSource(res1.data)
-      })
-      .catch((err) => console.log(err));
-  };
+  useEffect(() => { getInvoice(); }, [setVisible]);
 
   const DeleteInvoice = async () => {
     const token1 = await AsyncStorage.getItem("AccessToken");
     //console.log(token1)
+
     axios
-      .post(BASE_URL + "/delete_invoice/" + item["invoiceID"].toString(), {}, {
+      .post(BASE_URL + "/delete_invoice/" + item["invoiceID"].toString(),{}, {
         headers: { token: token1 }
       })
       .then(async (res1) => {
-        //console.log(res1.data)
-        if (res1.status == 200) {
+        if (res1.status === 200) {
           showMessage({
             message: res1.data,
             backgroundColor: "green",
@@ -52,45 +43,90 @@ const InvoiceDetail = ({ item, setVisible }) => {
             type: "error",
           });
         }
-
       })
-      .catch((err) => console.log(err));
   };
 
-  //need to edit
-  const savePDF = async () => {
-    const permissions = await StorageAccessFramework.requestDirectoryPermissionsAsync();
-    if (!permissions.granted) {
-      return;
-    }
+  const getInvoice = async () => {
+    const token1 = await AsyncStorage.getItem("AccessToken");
+    //console.log(token1)
 
+    axios
+      .get(BASE_URL + "/get_invoice/" + item["invoiceID"].toString() + "/base64", {
+        headers: { token: token1, responseType: 'arraybuffer' }
+      })
+      .then(async (res1) => {
+        //console.log(res1.data)
+        Setsource(res1.data)
+      })
+  };
+
+  const saveImage = async () => {
     try {
-      await StorageAccessFramework.createFileAsync(permissions.directoryUri,  item["invoiceID"].toString()+".pdf", 'application/pdf')
-        .then(async (uri) => {
-          await FileSystem.writeAsStringAsync(uri, source, { encoding: FileSystem.EncodingType.Base64 });
-        })
-        .catch((e) => {
-          console.log(e);
+      // Request device storage access permission
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status === "granted") {
+        const filename = FileSystem.documentDirectory + "invoice_" + item["invoiceID"] + ".jpg";
+        await FileSystem.writeAsStringAsync(filename, imageSource, {
+          encoding: FileSystem.EncodingType.Base64,
         });
-    } catch (e) {
-      throw new Error(e);
+
+        showMessage({
+          message: "Invoice Is Saved",
+          backgroundColor: "green",
+          type: "success",
+        });
+      }
+
+
+      const mediaResult = await MediaLibrary.saveToLibraryAsync(filename);
+    } catch (error) {
+      showMessage({
+        message: error,
+        backgroundColor: "red",
+        type: "error",
+      });
     }
   };
 
   return (
-    <View>
-      <Button onPress={() => setVisible(false)} appearance={"ghost"}>
-        Back
-      </Button>
-      <Button status='danger' onPress={() => DeleteInvoice()}>
-        Delete
-      </Button>
-      <Button onPress={() => savePDF()}>
-        Save To Device
-      </Button>
-    </View>
+    <Layout style={styles.page}>
+      <ScrollView>
+
+        <Image
+          source={{
+            uri: `data:image/jpeg;base64,${imageSource}`,
+          }}
+          style={{ resizeMode: "contain", width: 400, height: 500 }}
+        />
+
+        <Button status='success' onPress={() => saveImage()}>
+          Save
+        </Button>
+
+        <Button status='danger' onPress={() => DeleteInvoice()}>
+          Delete
+        </Button>
+
+        <Button onPress={() => setVisible(false)} appearance={"ghost"}>
+          Back
+        </Button>
+
+      </ScrollView>
+
+    </Layout>
   )
 
 }
+
+const styles = StyleSheet.create({
+  page: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  zoomableView: {
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+});
 
 export default InvoiceDetail
