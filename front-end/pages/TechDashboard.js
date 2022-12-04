d9828f8bf290620054124b38d5e975608a17f250
KimiNoNawa1610 <vothanhnhan108@gmail.com>
2022-12-04 13:17:05 -0800

    fix the jobs dashboard

--- front-end/pages/TechDashboard.js
+++ front-end/pages/TechDashboard.js
@@ -7,6 +7,7 @@ import ViewJobs from "./ViewJobs";
 export default function TechDashBoard() {
   //const navigation = useNavigation();
   const[active,setActive] = useState("Today")
+  const months = [1,2,3,4,5,6,7,8,9,10,11,12]
 
   return (
     <Layout style={styles.center} level="1">
@@ -25,10 +26,10 @@ export default function TechDashBoard() {
       </Layout>
 
       <Layout level="3">
-          {active === "Today" && <ViewJobs start={`${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`} end={`${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()+1}`} iscompleted={false}/>}
-          {active === "Past" && <ViewJobs start={`${new Date().getFullYear()}-${new Date().getMonth()+1-1}-${new Date().getDate()}`} end={`${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()-1}`} iscompleted={"None"}/>}
-          {active === "Future" && <ViewJobs start={`${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()+1}`} end={`${new Date().getFullYear()}-${new Date().getMonth()+2}-${new Date().getDate()}`} iscompleted={false}/>}
-          {active === "Completed" && <ViewJobs start={`${new Date().getFullYear()}-${new Date().getMonth()+1-1}-${new Date().getDate()}`} end={`${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`} iscompleted={true}/>}
+          {active === "Today" && <ViewJobs start={`${new Date().getFullYear()}-${months[new Date().getMonth()]}-${new Date().getDate()}`} end={`${new Date().getFullYear()}-${months[new Date().getMonth()]}-${new Date().getDate()+1}`} iscompleted={false}/>}
+          {active === "Past" && <ViewJobs start={`${new Date().getFullYear()}-${months[new Date().getMonth()] > 2 ? months[new Date().getMonth()]-1 : 12}-${new Date().getDate()}`} end={`${new Date().getFullYear()}-${months[new Date().getMonth()]}-${new Date().getDate()-1}`} iscompleted={"None"}/>}
+          {active === "Future" && <ViewJobs start={`${new Date().getFullYear()}-${months[new Date().getMonth()]}-${new Date().getDate()+1}`} end={`${new Date().getFullYear()}-${months[new Date().getMonth()] < 12 ? months[new Date().getMonth()] + 1 : 1}-${new Date().getDate()}`} iscompleted={false}/>}
+          {active === "Completed" && <ViewJobs start={`${new Date().getFullYear()}-${months[new Date().getMonth()] > 2 ? months[new Date().getMonth()] - 1 : 12}-${new Date().getDate()}`} end={`${new Date().getFullYear()}-${months[new Date().getMonth()]}-${new Date().getDate()}`} iscompleted={true}/>}
       </Layout>
     </Layout>
   );
